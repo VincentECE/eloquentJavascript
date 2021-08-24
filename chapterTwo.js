f700b24147a6e8e95cc2a1e583e274c2549481ec
VincentECE <vincent.tran.ece@gmail.com>
2021-08-24 18:33:49 -0400

    Initial commit

--- chapterTwo.js
+++ chapterTwo.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+/* Exercise from Eloquent Javascript 3rd edition Chapter 2 Exercise 1
+
+    Write a loop that makes seven calls to console.log to output the following triangle:
+
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+
+*/
+
+
+
+let strings = "";
+
+for(let i=0; i<=6; i++){
+  strings = strings + '#';
+  console.log(strings);
+}
